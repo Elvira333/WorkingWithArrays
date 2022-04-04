a31a012574f807fbf8acaf09d5bc6ea7251b8f52
Elvira <elviraw@mail.ru>
2022-04-04 14:31:42 +0300

    add task

---
+++ task/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] value = { "15", "hello", "600", "4", "control Work", "by", "Annaeva" };
+void Examination(string[] values)
+{
+    int count = 0;
+    string[] valuesTwo = new string[values.Length];
+    for (int i = 0; i < values.Length; i++)
+    {
+        string number = values[i];
+        if (number.Length <= 3)
+        {
+            valuesTwo[count] = number;
+            Console.Write($"{valuesTwo[count]} ");
+            count++;
+        }
+    }
+}
+Examination(value);
