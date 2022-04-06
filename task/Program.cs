1ece2d63658d8858bb06d799625342649bd5c534
Elvira <elviraw@mail.ru>
2022-04-06 15:06:43 +0300

    changed task

--- task/Program.cs
+++ task/Program.cs
@@ -3,16 +3,30 @@ string[] value = { "15", "hello", "600", "4", "control Work", "by", "Annaeva" };
 void Examination(string[] values)
 {
     int count = 0;
-    string[] valuesTwo = new string[values.Length];
+    int j = 0;
     for (int i = 0; i < values.Length; i++)
     {
         string number = values[i];
         if (number.Length <= 3)
         {
-            valuesTwo[count] = number;
-            Console.Write($"{valuesTwo[count]} ");
             count++;
         }
+
     }
+    string[] valuesTwo = new string[count];
+    
+    for (int i = 0; i < values.Length; i++)
+    {
+        
+        string number = values[i];
+        if (number.Length <= 3)
+        {
+            valuesTwo[j] = number;
+            Console.Write($"{valuesTwo[j]} ");
+            j++;
+        }
+
+    }
+
 }
 Examination(value);
